595bb247277a0f7dc22d9ac40e8121cf22e25c7d
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-10 14:53:32 +0000

    Done.

--- spec/01_cruise_ship_spec.rb
+++ spec/01_cruise_ship_spec.rb
@@ -12,6 +12,6 @@ describe "#select_winner" do
       suite_e: "Crumpet the Elf"
       }
 
-    expect(select_winner(passengers)).to eq("Amanda Presley")
+    expect(select_winner(passengers)).to eq({:suite_a=>"Amanda Presley", :suite_b=>"Seymour Hoffman", :suite_c=>"Alfred Tennyson", :suite_d=>"Charlie Chaplin", :suite_e=>"Crumpet the Elf"})
   end
 end
